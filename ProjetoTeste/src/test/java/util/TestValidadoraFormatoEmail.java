555fdf3553c3f9a4250effa3ab7e4452d15a4e63
PedroSCY <hock.gonzagas7@gmail.com>
2021-12-14 23:15:05 -0300

    restauração

--- ProjetoTeste/src/test/java/util/TestValidadoraFormatoEmail.java
+++ ProjetoTeste/src/test/java/util/TestValidadoraFormatoEmail.java
@@ -1,20 +1,15 @@
 package util;
 
-
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
-
-
 
 public class TestValidadoraFormatoEmail {
 
 	@Test
 	public void testValidarEmailComEmailValido() {
 		String teste = "admin@admin.com";
-		
 		assertTrue(ValidadoraFormatoEmail.validarEmail(teste));
 	}
 	
